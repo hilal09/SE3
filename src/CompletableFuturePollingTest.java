f3b03f2489cfa8b51c4896a2a359794f49d94e44
hilal09 <hilal.cubukcu02@gmail.com>
2024-11-10 17:04:55 +0100

    3b

--- src/CompletableFuturePollingTest.java
+++ src/CompletableFuturePollingTest.java
@@ -4,26 +4,27 @@ import java.util.concurrent.*;
 
 public class CompletableFuturePollingTest extends Thread {
 
-    private int counter = 0;
-
     public static void main(String[] args) {
         new CompletableFuturePollingTest(); 
     }
     public CompletableFuturePollingTest() {
         this.start();
     }
+    @Override
     public void run() {
        
         BMIRechner bmi = new BMIRechner();
 
         CompletableFuture.supplyAsync(() -> {
-            getRandomSleepTime(1000,3000);
             double bmiValue = bmi.bmiBerechnen();
-            System.out.println("Thread: " + Thread.currentThread().getName() + " - BMI-Wert: " + bmiValue);
-            return bmiValue;})
+                getRandomSleepTime(1000, 3000);
+                System.out.println("Thread: " + Thread.currentThread().getName() + " - BMI-Wert: " + bmiValue);
+                return bmiValue;
+            })
             .thenApplyAsync(s -> {
-                bmi.diagnose(s);
-                return ??;
+                String r = bmi.diagnose(s);
+                getRandomSleepTime(1000, 3000);
+                return r;
             })                
             .thenAccept(result -> System.out.println(Thread.currentThread() + " Diagnose: " + result))
             .join(); 
@@ -39,5 +40,6 @@ public class CompletableFuturePollingTest extends Thread {
         }
         
     }
+
 }
     
