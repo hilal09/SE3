84b4df5ddb645b242e04dc48f7a586eaf54390c8
Sunmin Yook <sunminyook.01@gmail.com>
2024-11-10 15:39:49 +0100

    polling done

--- src/PollingCaller.java
+++ src/PollingCaller.java
@@ -1,23 +1,54 @@
 
-import java.io.IOException;
+// import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Random;
 
 public class PollingCaller extends Thread {
-    private boolean returned = false;
 
-    public PollingCaller () { 
+    private boolean returned = false;
+    private int daysUntilNextBday = 0;
+
+    public PollingCaller() { 
         this.start();  
     }
 
+    //geburtstag berechnen
     public void run() {
-        System.out.println(" entfernter Aufruf läuft u. wartet auf Taste ");
-    try {
-        System.in.read();
-    } catch (IOException e) { e.printStackTrace(); }
+
+        LocalDate currentDate = LocalDate.now();
+        //kommender geburtstag hard coded
+        LocalDate bday = LocalDate.of(2024, 12, 29);
+
+        while (!currentDate.equals(bday)) { 
+            daysUntilNextBday++;
+            currentDate = currentDate.plusDays(1);   
+        }
+
+        //warten
+        Random rand = new Random();
+        // random.nextInt(max - min + 1) + min : zufaellige zahl zwischen null und angegebene zahl-1
+        int randomNum = rand.nextInt(7) + 4; //zufaellige zahl zwischen 4-10
+        try {
+            sleep(randomNum * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // System.out.println(" entfernter Aufruf läuft u. wartet auf Taste ");
+        // try {
+        //     System.in.read();
+        // } catch (IOException e) { 
+        //     e.printStackTrace(); 
+        // }
         returned = true;
     }
     
     public boolean returned() { 
         return returned; 
+    }
+
+    public int getDaysUntilNextBday() {
+        return daysUntilNextBday;
     }
 
 }
--- src/PollingClient.java
+++ src/PollingClient.java
@@ -1,21 +1,47 @@
+import java.util.Random;
+
 public class PollingClient {
     public static void main(String[] args) {
-        int start = 0;
-        TageAusrechnen bday = new TageAusrechnen();
+        // int start = 0;
+        // TageAusrechnen bday = new TageAusrechnen();
         PollingCaller caller = new PollingCaller();
-        while (caller.returned() == false) { // Polling
-            System.out.print((start += 1) + ", "); // Zählerausgabe
+        Random rand = new Random();
+        
+        //Zählerausgabe
+        int zahl = 0;
+        System.out.print(zahl); 
+        while (!caller.returned()){
             try {
-                Thread.sleep(2000);
+                Thread.sleep((rand.nextInt(3) + 1) * 1000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                //wie kommt man in diesen block? wenn stop, dann interrupt 
+                //(falls gerade der main thread sleep ist) damit zaehlerausgabe stoppt. 
+                //(in diesem fall ohne wiederholte if abfrage fuer stop)
+                System.out.println("Zaehlerausgabe beendet.");
+                // e.printStackTrace();
             }
+            if (!caller.returned()) {
+                System.out.println(", ");
+                System.out.print(++zahl); // ++zahl, weil sonst zwei mal die null ausgegeben wird
+            } else break;
         }
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(bday.tageausrechnen());
+        
+        System.out.println();
+        System.out.println("Es verbleiben " + caller.getDaysUntilNextBday() + " Tage bis zu deinem Geburtstag.");
+        
+        // while (caller.returned() == false) { // Polling
+        //     System.out.print((start += 1) + ", "); // Zählerausgabe
+        //     try {
+        //         Thread.sleep(2000);
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // }
+        // try {
+        //     Thread.sleep(4000);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        // System.out.println(bday.tageausrechnen());
     }
 }
